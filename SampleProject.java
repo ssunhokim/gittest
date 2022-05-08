73969d1e6467024e1b78d8cf781ddecc323c32bf
ssunhokim <algorithm887@naver.com>
2022-05-08 22:35:22 +0900

    Feature (#1)
    * [CRM-123] git test >> 01
    
    * [CRM-123] git test >> 02
    
    * [CRM-123] git test >> 02
    
    Co-authored-by: 김 선호 <sunho.kim@iarkdata.com>

--- SampleProject.java
+++ SampleProject.java
@@ -5,5 +5,8 @@ public class SampleProject {
         int test01 = 1234;
         int test02 = 1234;
         int test03 = 1234;
+
+        int test05 = 12123134;
+        int test06 = 1234;
     }
 }
